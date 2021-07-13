3aab5802ab61fcaeb6ce51150ba3203a0cb4609b
Andrew <andrewoshei@gmail.com>
2021-07-13 02:47:27 +0200

    Added BLE scan and connect functionality to Android app remix

--- Messenger_BLE/app/src/main/java/com/example/messenger_ble/MainActivity.kt
+++ Messenger_BLE/app/src/main/java/com/example/messenger_ble/MainActivity.kt
@@ -2,34 +2,47 @@ package com.example.messenger_ble
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.app.AlertDialog
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothDevice
+import android.app.Dialog
+import android.bluetooth.*
+import android.bluetooth.BluetoothDevice.TRANSPORT_LE
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.bluetooth.le.ScanSettings
+import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.content.res.ColorStateList
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import android.os.*
 import android.util.Log
+import android.view.Window
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import androidx.activity.viewModels
 import com.example.messenger_ble.adapters.ListDataAdapter
 import com.example.messenger_ble.data.ClickApplication
 import com.example.messenger_ble.data.ListData
 import com.example.messenger_ble.databinding.ActivityMainBinding
-import com.example.messenger_ble.network.BluetoothClient
 import com.example.messenger_ble.ui.MainViewModel
 import com.example.messenger_ble.ui.MainViewModelFactory
 import com.example.messenger_ble.utilities.`Encryption_AES128-GCM`.parseEncrypt
 import com.example.messenger_ble.utilities.errorDialog
 import com.example.messenger_ble.utilities.messageToast
-import java.util.ArrayList
-import java.util.HashMap
+import java.util.*
+
 
 // Declare string, pour passer des messages de MainActivity à BluetoothClient Class
 var message = ""
@@ -44,6 +57,7 @@ var newMessage = ""
 // ByteArray for outgoing messages
 var outgoing: ByteArray = ByteArray(1)
 
+@kotlin.ExperimentalUnsignedTypes
 var mCounter: UByte = 1.toUByte()
 
 class MainActivity : AppCompatActivity() {
@@ -60,11 +74,12 @@ class MainActivity : AppCompatActivity() {
 
     // Déclarer la baie contient l'ID de périphérique Bluetooth
     private var devices = ArrayList<BluetoothDevice>()
-    // Déclarer HashMap contient les adresses de l'appareil Bluetooth
-    private var devicesMap = HashMap<String, BluetoothDevice>()
     // Déclarer l'adaptateur Array, contient des chaînes pour Bluetooth AlertDialog
     private var mArrayAdapter: ArrayAdapter<String>? = null
 
+    lateinit var devCounter : TextView
+
+    lateinit var bluetoothGatt: BluetoothGatt
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -100,63 +115,29 @@ class MainActivity : AppCompatActivity() {
     private fun bluetoothPermissions() {
         // Si les autorisations Bluetooth ne sont pas définies
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION)
+                        Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
             // Demandez l'autorisation d'accéder au Bluetooth lors du premier lancement de l'application
             ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
         }
     }
 
 
     private fun bindButtonListeners() {
-        binding.buttonConn.setOnClickListener() { _ ->
-            // Si le Bluetooth n'existe pas sur l'appareil
-            if (BluetoothAdapter.getDefaultAdapter() == null) {
-                messageToast(this, "Bluetooth is disabled!")
-            }
-            // Vérifiez que le Bluetooth est activé
-            else if(!BluetoothAdapter.getDefaultAdapter().isEnabled){
-                // Afficher un message d'erreur
-                errorDialog(this, "Bluetooth is not enabled,\nEnable bluetooth and retry.")
+        binding.buttonConn.setOnClickListener {
+            if (isScanning) {
+                stopBleScan()
             } else {
-                // Initialiser la variable d'appareil, stocke temporairement l'appareil de BluetoothAdapter
-                devices = ArrayList()
-                // Réinitialiser la liste des appareils
-                mArrayAdapter!!.clear()
-                // Si la connexion est active
-                if(connexion) {
-                    // Load outgoing message, set variables and UI ELements to close the connection
-                    setDisconnect()
-                    // Si la connexion n'est pas active
-                } else {
-                    // Load outgoing message, reset variables and UI Elements to initiate a connection
-                    setConnect()
-                    // Load paired devices into bluetooth adapter
-                    loadBluetoothAdapter()
-                    // Vérifiez qu'il existe des appareils couplés avant la découverte, sinon l'application se bloque
-                    if(devicesMap.isNotEmpty()) {
-                        // If devices are available prompt user to choose a device
-                        discoverBluetoothDevices()
-                    } else {
-                        // Déclarer la connexion fermée
-                        connexion = false
-                        // Modifier le texte sur le bouton de connexion
-                        binding.buttonConn.text = "Connect"
-                        // Mettre à jour le journal des messages
-                        errorDialog(this, "No paired devices detected!")
-                        // Déverrouille le bouton de connexion
-                        unlockButton()
-                    }
-                }
+                startBleScan()
             }
         }
 
-        binding.buttonSend.setOnClickListener() {
+        binding.buttonSend.setOnClickListener {
             sendMessage()
         }
 
-        binding.buttonClear.setOnClickListener() {
+        binding.buttonClear.setOnClickListener {
             if (binding.editOutput.text.toString() == "") {
                 clearDatabase()
             } else {
@@ -164,12 +145,13 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        binding.buttonOnline.setOnClickListener() {
+        binding.buttonOnline.setOnClickListener {
             messageToast(this, connectionStatus)
         }
     }
 
 
+    @kotlin.ExperimentalUnsignedTypes
     private fun sendMessage() {
         newMessage = binding.editOutput.text.toString()
         if (connexion) {
@@ -194,6 +176,8 @@ class MainActivity : AppCompatActivity() {
         binding.editOutput.setText("")
     }
 
+
+    @kotlin.ExperimentalUnsignedTypes
     private fun setConnect() {
         clearDatabase()
         // Définir le message de connexion
@@ -212,6 +196,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @kotlin.ExperimentalUnsignedTypes
     private fun setDisconnect() {
         // Définir le message de déconnexion
         message = "<DISCONN>"
@@ -243,48 +228,10 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private fun loadBluetoothAdapter() {
-        // Effacer le hashmap, évite les erreurs si l'appareil n'est pas couplé pendant que l'application est active
-        devicesMap = HashMap<String, BluetoothDevice>()
-        // Pour tous les appareils couplés dans l'adaptateur Bluetooth
-        for (device in BluetoothAdapter.getDefaultAdapter().bondedDevices) {
-            // Charger l'appareil avec l'adresse associée dans la carte des appareils
-            devicesMap[device.address] = device
-            // Ajouter un appareil à la liste des appareils
-            devices.add(device)
-            // Ajoutez le nom et l'adresse à un adaptateur de baie à afficher dans un ListView
-            mArrayAdapter!!.add((if (device.name != null) device.name else "Unknown") + "\n" + device.address + "\nPaired")
-        }
-    }
-
-
-    private fun discoverBluetoothDevices() {
-        // Lancer le processus de découverte
-        if (BluetoothAdapter.getDefaultAdapter().startDiscovery()) {
-            // Configurer le générateur AlertDialog
-            // Cette boîte de dialogue permet à l'utilisateur de sélectionner parmi les appareils Bluetooth couplés
-            val dialog: AlertDialog.Builder = AlertDialog.Builder(this)
-            // Définir le titre AlertDialog
-            dialog.setTitle("Connect to: ")
-            // Ne pas autoriser l'annulation des dialogues, cela évite un bug de faux état
-            dialog.setCancelable(false)
-            // Créer la liste des appareils couplés à partir de l'adaptateur et de l'action de liaison
-            dialog.setAdapter(mArrayAdapter) { _, which: Int ->
-                // Terminer le processus de découverte Bluetooth
-                BluetoothAdapter.getDefaultAdapter().cancelDiscovery()
-                // Démarrer le thread client Bluetooth avec le périphérique sélectionné
-                BluetoothClient(this, devices[which]).start()
-            }
-            // Rendre le AlertDialog
-            dialog.show()
-        }
-    }
-
-
-    // Internationalisation Seppress, nous voulons une sortie brute et non une traduction
-    @SuppressLint("SetTextI18n")
-    // This function receives and pasres bluetooth messages
+    // Receive and parse bluetooth messages
     // TODO Messages should be decrypted before arriving at this function
+    @SuppressLint("SetTextI18n")
+    @kotlin.ExperimentalUnsignedTypes
     fun parseMessage(text: Pair<String, UByte>) {
         msgCount += 1
         var data = ListData(0, msgCount, "IN", text.first)
@@ -348,9 +295,239 @@ class MainActivity : AppCompatActivity() {
         mViewModel.delete()
     }
 
+    // Setup BLE BLuetooth Adapter
+    private val bluetoothAdapter: BluetoothAdapter by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    // Setup BLE Scanner
+    private val bleScanner by lazy {
+        bluetoothAdapter.bluetoothLeScanner
+    }
+
+
+    // Setup BLE Scanner Settings
+    private val scanSettings = ScanSettings.Builder()
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY).build()
+
+
+    /*
+    // Declare UUID for scan filter
+    private val UART_SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+
+    // Filter for BLE Scan
+    val filterUART = ScanFilter.Builder().setServiceUuid(
+        ParcelUuid.fromString(UART_SERVICE_UUID)
+    ).build()
+    */
+
+
+    // On Resume check that Bluetooth is Enabled, else prompt user
+    override fun onResume() {
+        super.onResume()
+        if (!bluetoothAdapter.isEnabled) {
+            promptEnableBluetooth()
+        }
+    }
+
+
+    private var resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode != Activity.RESULT_OK) {
+            promptEnableBluetooth()
+        }
+    }
+
+    private fun promptEnableBluetooth() {
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            resultLauncher.launch(enableBtIntent)
+        }
+    }
+
+
+    // For requesting fine location access on BLE Scan
+    private val isLocationPermissionGranted
+        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+
+    private fun Context.hasPermission(permissionType: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permissionType) ==
+                PackageManager.PERMISSION_GRANTED
+    }
+
+
+    // If permissions are properly set, Start scanning for BLE Devices
+    private fun startBleScan() {
+        if (!isLocationPermissionGranted) {
+            messageToast(this, "Location access required\n to scan BLE devices.")
+        }
+        else {
+            devices.clear()
+            mArrayAdapter!!.clear()
+            bleScanner.startScan(null, scanSettings, scanCallback)
+            isScanning = true
+            val dialog = Dialog(this)
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+            dialog.setCancelable(false)
+            dialog.setContentView(R.layout.scan_dialog)
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+            devCounter = dialog.findViewById(R.id.textCount) as TextView
+            val pip1 = dialog.findViewById(R.id.imagePip1) as ImageView
+            val pip2 = dialog.findViewById(R.id.imagePip2) as ImageView
+            val pip3 = dialog.findViewById(R.id.imagePip3) as ImageView
+            val pip4 = dialog.findViewById(R.id.imagePip4) as ImageView
+            val pip5 = dialog.findViewById(R.id.imagePip5) as ImageView
+            val a: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_anim)
+            a.duration = 3000
+            pip1.startAnimation(a)
+            val b: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_anim)
+            b.duration = 2800
+            b.startOffset = 200
+            val c: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_anim)
+            c.duration = 2600
+            c.startOffset = 400
+            val d: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_anim)
+            d.duration = 2400
+            d.startOffset = 600
+            val e: Animation = AnimationUtils.loadAnimation(this, R.anim.progress_anim)
+            e.duration = 2200
+            e.startOffset = 800
+            pip1.startAnimation(a)
+            pip2.startAnimation(b)
+            pip3.startAnimation(c)
+            pip4.startAnimation(d)
+            pip5.startAnimation(e)
+            val yesBtn = dialog.findViewById(R.id.buttStopScan) as Button
+            yesBtn.setOnClickListener {
+                dialog.dismiss()
+                stopBleScan()
+            }
+            dialog.show()
+        }
+    }
+
+
+    // Stop the BLE Scan
+    private fun stopBleScan() {
+        bleScanner.stopScan(scanCallback)
+        isScanning = false
+        if (devices.isNotEmpty()) {
+            val dialog: AlertDialog.Builder = AlertDialog.Builder(this)
+            dialog.setTitle("Select Device:")
+            dialog.setCancelable(false)
+            dialog.setAdapter(mArrayAdapter) { _, which: Int ->
+                btStart(this, devices[which])
+            }
+            dialog.setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            dialog.show()
+        } else {
+            errorDialog(this, "No devices detected!")
+        }
+    }
+
+
+    private fun btStart(c: Context, device: BluetoothDevice) {
+        val name = device.name
+        val addr = device.address
+        Log.d("SelectDevice", "$name, $addr ")
+        device.connectGatt(c, false, gattCallback, TRANSPORT_LE)
+    }
+
+
+    // Scan results are sent to callback when discovered
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            if (!devices.contains(result.device)) {
+                with(result.device) {
+                    Log.i("ScanCallback", "Found BLE Device - " +
+                            "Name: ${name ?: "UnNamed"}, address: $address")
+                }
+                devices.add(result.device)
+                var dCnt = devices.size
+                devCounter.text = dCnt.toString()
+                mArrayAdapter!!.add((if (result.device.name != null)
+                    result.device.name else "Unknown") + "\n" +
+                        result.device.address + "\n" +
+                        "RSSI: " + result.rssi)
+            }
+        }
+        override fun onScanFailed(errorCode: Int) {
+            Log.e("ScanCallback", "onScanFailed: code $errorCode")
+        }
+    }
+
+
+    // If scanning for BLE Devices toggle scan button label
+    private var isScanning = false
+        //Save Setter example for future use
+        //set(value) {
+        //    field = value
+        //    runOnUiThread { binding.buttonConn.text = if (value) "Stop Scan"
+        //                    else "Start Scan" }
+        //}
+
+
+    // CHECK HERE TRY TO FIND WHY CONNECTION FAILS
+    private val gattCallback = object : BluetoothGattCallback() {
+        override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
+            val deviceAddress: String = gatt.device.address
+            Log.d("BT_STATUS", status.toString())
+            if (status == BluetoothGatt.GATT_SUCCESS) {
+                if (newState == BluetoothProfile.STATE_CONNECTED) {
+                    Log.w("BluetoothGattCallback",
+                            "Successfully connected to $deviceAddress")
+                    this@MainActivity.bluetoothGatt = gatt
+                    Handler(Looper.getMainLooper()).post {
+                        bluetoothGatt.discoverServices()
+                    }
+                } else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+                    Log.w("BluetoothGattCallback",
+                            "Successfully disconnected from $deviceAddress")
+                    gatt.close()
+                }
+            } else {
+                Log.w("BluetoothGattCallback",
+                        "Error $status encountered for $deviceAddress! Disconnecting...")
+                gatt.close()
+            }
+        }
+        override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
+            with(gatt) {
+                Log.w("BluetoothGattCallback",
+                        "Discovered ${services.size} services for ${device.address}")
+                printGattTable()
+            }
+        }
+    }
+
+
+    // Function for printing available Bluetooth Services
+    private fun BluetoothGatt.printGattTable() {
+        if (services.isEmpty()) {
+            Log.i("printGattTable",
+                    "No service and characteristic available, call discoverServices() first?")
+            return
+        }
+        services.forEach { service ->
+            val characteristicsTable = service.characteristics.joinToString(
+                    separator = "\n|--",
+                    prefix = "|--"
+            ) { it.uuid.toString() }
+            Log.i("printGattTable",
+                    "\nService ${service.uuid}\nCharacteristics:\n$characteristicsTable")
+        }
+    }
+
+
+    // On close activity
     override fun onDestroy() {
         super.onDestroy()
-        setDisconnect()
+        if (isScanning) {
+            bleScanner.stopScan(scanCallback)
+            isScanning = false
+        }
     }
 }
 
